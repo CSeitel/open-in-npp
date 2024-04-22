33b1313684883402c5477ea620f6b1c2f129ee43
CSeitel <43964178+CSeitel@users.noreply.github.com>
2024-04-23 00:54:40 +0200

    more

--- src/test/suite/all.spec.ts
+++ src/test/suite/all.spec.ts
@@ -16,86 +16,71 @@
   import * as ß_fs  from './lib/fs.vsc' ;
 //====================================================================
   type TTestSuite           = Record<string,TAsyncTestFunction>
-  type TTestSuiteDefinition = [string,TTestSuite,boolean,TTestSuite]
+  type TTestSuiteDefinition = [string,TTestSuite,boolean]
   type TTestSuites          = TTestSuiteDefinition[]
 //--------------------------------------------------------------------
-    const ß_store:Record<number,TTestSuite> =
-    { 0: { 0: ö_when, 1: ö_when, 2: ö_when, 3: ö_when, 4: ö_when, 5: ö_when, 6: ö_when, 7: ö_when, 8: ö_when, 9: ö_when }
-    , 1: { 0: ö_when, 1: ö_when, 2: ö_when, 3: ö_when, 4: ö_when, 5: ö_when, 6: ö_when, 7: ö_when, 8: ö_when, 9: ö_when }
-    , 2: { 0: ö_when, 1: ö_when, 2: ö_when, 3: ö_when, 4: ö_when, 5: ö_when, 6: ö_when, 7: ö_when, 8: ö_when}
-    };
-  const ß_store_0:TTestSuite = { 0: ö_when, 1: ö_when, 2: ö_when, 3: ö_when, 4: ö_when, 5: ö_when, 6: ö_when, 7: ö_when, 8: ö_when, 9: ö_when };
-  const ß_store_1:TTestSuite = { 0: ö_when, 1: ö_when, 2: ö_when, 3: ö_when, 4: ö_when, 5: ö_when, 6: ö_when, 7: ö_when, 8: ö_when, 9: ö_when };
-  const ß_store_2:TTestSuite = { 0: ö_when, 1: ö_when, 2: ö_when, 3: ö_when, 4: ö_when, 5: ö_when, 6: ö_when, 7: ö_when, 8: ö_when };
-//--------------------------------------------------------------------
-  const ß_single = { dispatch: ß_fs.tst_dispatch
-                   , abcee   : ß_fs.tst_whenFileInfoRead
-                   , bcee   : ß_fs.tst_whenFileInfoRead
+  const ß_single = { adispatch: ß_fs.tst_dispatch
+                   , bbcee   : ß_fs.tst_whenFileInfoRead
+                   , ccee   : ß_fs.tst_whenFileInfoRead
                    } as TTestSuite;
 //--------------------------------------------------------------------
+  const ß_proxy     = [] as TAsyncTestFunction[][];
   const ß_debugUI   =                      true;
-  const ß_skipTests = ß_debugUI ? false : !true; // = except single test
-  const ü_suites = (
-    [ [ 'Single',  ß_etc   ,  ß_skipTests, ß_store_0 ]
-    , [ 'Etc'   ,  ß_fs    ,  ß_skipTests, ß_store_1 ]
-    , [ 'Fs'    ,  ß_fs    ,  ß_skipTests, ß_store_2 ]
+  const ß_skipTests =  true; // = except single test
+  const ß_suites = (
+    [ [ 'Single',  ß_single,  ß_debugUI ? false :
+                             !ß_skipTests ]
+    , [ 'Etc'   ,  ß_etc   ,  ß_skipTests ]
+    , [ 'Fs'    ,  ß_fs    ,  ß_skipTests ]
     ] as TTestSuites
-    ).filter( ü_suite => !ü_suite[2] )
- // .slice(0)
-   ;
+  //).filter( ü_suite => !ü_suite[2] )
+    ;
+//====================================================================
+
+  if ( ß_debugUI ) {
+      ß_expandAllTests( 0 );
+      ß_expandAllTests( 1 );
+      ß_expandAllTests( 2 );
+
+                          //ü_suites.forEach( ß_expandTestSuite );
+  } else { whenAllTestsRun( ß_suites as any ); }
+
+//====================================================================
+
+function ß_expandAllTests( ü_sIndx:number ):void {
+    const ö_suite = ß_suites[ ü_sIndx ];
+    suite( ''+ü_sIndx, function(){
+        if ( ö_suite[2] ) { return; }
+           ß_expandTestSuite( ö_suite, ü_sIndx );
+        test( '0', async function(){ return ß_when(ü_sIndx,0); });
+        test( '1', async function(){ return ß_when(ü_sIndx,1); });
+        test( '2', async function(){ return ß_when(ü_sIndx,2); });
+        test( '3', async function(){ return ß_when(ü_sIndx,3); });
+        test( '4', async function(){ return ß_when(ü_sIndx,4); });
+    });
+}
+
 //--------------------------------------------------------------------
 
-  if ( ß_debugUI ) {        
-                            ü_suites.forEach( ß_expandTestSuite );
-  } else { whenAllTestsRun( ü_suites as any ); }
-
-//====================================================================
-
-async function ö_when(){
-}
-
-function ß_wrap_( ö_tests:TTestSuite, ü_store:TTestSuite ):TTestSuite {
-    const ü_testKeys = ( Object.keys( ü_store ) as (keyof typeof ü_store )[] ).sort();
-    for ( const ü_testName in ö_tests ) {
-        const ü_testKey = ü_testKeys.shift();
-        if ( ü_testKey === undefined ) { continue; }
-        ü_store[ ü_testKey ] = ö_wrap( ö_tests[ ü_testName ] );
-    }
-  //
-        const ü_testKey = ü_testKeys.shift()!;
-    delete ü_store[ ü_testKey ]
-  //ü_testKeys.forEach(function( ü_mKey ){  delete ü_store[ parseInt( ü_mKey ) ];  });
-  //
-    return ü_store;
-function ö_wrap( ö_testImpl:TAsyncTestFunction ):TAsyncTestFunction {
-    return ö_wrap;
+function ß_expandTestSuite( ü_suite:TTestSuiteDefinition, ü_indx:number ):void {
+    const ö_proxy = ß_proxy[ ü_indx ] = [] as TAsyncTestFunction[];
+    const ö_tests = ü_suite[1];
+    const ü_testKeys = ( Object.keys( ö_tests ) as (keyof typeof ö_tests)[] ).sort();
+    ü_testKeys.forEach(function( ü_testName, ü_indx ){
+        const ö_testImpl = ö_tests[ ü_testName ];
+        ö_proxy[ ü_indx ] = ö_wrap;
 function ö_wrap():PromiseLike<void> {
-    return ö_testImpl().then( testSummary as any ); }
+    return ö_testImpl().then( testSummary as any );
 }
+    });
 }
 
 //====================================================================
 
-function ß_expandTestSuite( ü_suite:TTestSuiteDefinition ):void {
-  /*
-    const ö_mKey = ß_mKeys.shift()!;
-    let ü_store = ß_store[ ö_mKey as any ];
-    switch ( parseInt( ö_mKey ) ) {
-        case 0: ü_store = ß_store_0; break;
-        case 1: ü_store = ß_store_1; break;
-        case 2: ü_store = ß_store_2; break;
-    }
-  */
-    const ö_tests = ß_wrap_( ü_suite[1], ü_suite[3] );
-    suite( ü_suite[0], ö_suiteRecord );
-  //
-function ö_suiteRecord():void {
-    for ( const ü_testName in ö_tests ) {
-        test(          ü_testName
-            , ö_tests[ ü_testName ]
-            );
-    }
-}
+async function ß_when( ü_suite:number, ü_test:number ){
+    const ü_whenTest = ß_proxy[ ü_suite ][ ü_test ];
+    if ( ü_whenTest === undefined ) { throw new Error( ''+ü_test ) }
+    await ü_whenTest();
 }
 
 //====================================================================
