33cf5b8a591ee845d2541eefa618b2c75f9d8037
CSeitel <christian.seitel@sap.com>
2024-05-08 17:02:49 +0200

    sap

--- etc/syntax.js
+++ etc/syntax.js
@@ -1,33 +1,40 @@
-const ü_s = require('fs').promises
+const fsProm = require('fs').promises;
 
-async function tst_syntax(){
-    const ü_p = Promise.resolve()
-    ü_p.then(function(){ 
-        throw new Error('5');
+async function codeWithUnhandledPromise(){
+    const notAwaited = Promise.resolve().then(function(){
+      //throw new Error('Irrelevant');
     });
+    const ss = await fsProm.stat( __filename );
+    console.log( ss, 'fs Not reached' );
+}
+async function main(){
+    try {
+      await codeWithUnhandledPromise();
+    } catch (error) {
+      console.log( 'Not reached' );
+    } finally {
+      console.log( 'Not Reached' );
+    }
+}
+
+main().then(undefined,function(){ 
+    console.log( 'Not Reached' );
+    return;
+});
+console.log( 'done' );
+
+//tst_syntax();
   //await some_tick();
+/*
+    const ü_p = Promise.resolve()
     const ü_6 = ü_p.then(function(){
         return;
     });
     await ü_6;
-    await ü_s.stat( __filename )
-    console.log( '6' )
+*/
 async function some_tick() {
                                      let ü_r
     const ü_p = new Promise((resolve)=>{ ü_r = resolve; });
                              setTimeout( ü_r, 1 );
     return ü_p;
 }
-}
-async function wrap(){
-    try {
-      await tst_syntax();
-        
-    } catch (error) {
-      return;
-    }
-}
-
-wrap();
-console.log( 'done' );
-//tst_syntax();
